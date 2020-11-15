ebf1f1b3b21d44d982b3ef6d399e514833dd2d7c
isaccanedo <isaccanedo@gmail.com>
2020-11-15 11:35:19 -0300

    Initial commit

---
+++ src/br/com/isaccanedo/Main.java
@@ -0,0 +1,29 @@
+package br.com.isaccanedo;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+
+        cifrar();
+
+    }
+
+    public static void cifrar() throws NoSuchAlgorithmException {
+        Date date = new Date();
+        SimpleDateFormat f = new SimpleDateFormat(("dd-MM-yyyy hh:mm:ss"));
+        String dataAtual = f.format(date);
+
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.reset();
+        md.update(dataAtual.getBytes());
+        System.out.println("O Hash para a data atual  " + dataAtual + " é:");
+        System.out.println(new BigInteger(1, md.digest()).toString(16));
+    }
+
+}
